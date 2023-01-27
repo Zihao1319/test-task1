72e96a658dabf9dfed026ddf753218338871e601
Zihao1319 <zihao1319@gmail.com>
2023-01-27 09:57:48 +0800

    file scanner and punctuations remover done

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,9 +1,45 @@
 package task1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
 
-    } 
+        // getting the file path and convert them to file
+
+        Path textFilePath = Paths.get(args[0]);
+        // System.out.println(textFilePath);
+
+        File textFile = textFilePath.toFile();
+        // System.out.println(textFile);
+
+        if (!textFile.exists()) {
+            System.out.println("File doesn't exist");
+        }
+
+        Scanner input = new Scanner(textFile);
+
+        int totalCounter = 0;
+
+        while (input.hasNext()) {
+            String word = input.next();
+
+            // removing all punctuation marks from words
+            String newWord = word.replaceAll("\\p{Punct}", "");
+            System.out.println(newWord);
+
+            //counting all the characters count
+            totalCounter += 1;
+
+
+
+        }
+
+    }
 }
---
+++ src/task1/Word.java
@@ -0,0 +1,22 @@
+package task1;
+
+public class Word {
+
+    private int count;
+
+    public Word () {};
+
+    public Word(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+        
+
+}
